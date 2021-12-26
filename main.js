e8c01f9a04d2561b174737988616aefed316bb94
thakurumayamah <93484148+thakurumayamah@users.noreply.github.com>
2021-12-26 13:54:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+rover_width = 100;
+rover_height = 90;
+
+background_img = "mars.jpg";
+
+rover_img = "rover.png";
+
+rover_x = 10;
+rover_y = 10;
+
+function add(){
+    background_imgTag = new Image();
+    background_imgTag.onload = uploadbackground;
+    background_imgTag.src = background_img;
+
+    rover_imgTag = new Image();
+    rover_imgTag.onload = uploadrover;
+    rover_imgTag.src = rover_img;
+}
+
+function uploadbackground(){
+    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadrover(){
+    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed == '38')
+    {
+        up();
+        console.log("up");
+    }
+    if(keyPressed == '40')
+    {
+        down();
+        console.log("down");
+    }
+    if(keyPressed == '37')
+    {
+        left();
+        console.log("left");
+    }
+    if(keyPressed == '39')
+    {
+        right();
+        console.log("right");
+    }
+}
